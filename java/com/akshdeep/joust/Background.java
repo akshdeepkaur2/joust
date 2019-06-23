f3f6481f80a41acb619c58cf73bacb310c1af008
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-06-23 00:51:54 -0400

    animation class added

---
+++ java/com/akshdeep/joust/Background.java
@@ -0,0 +1,4 @@
+package com.akshdeep.joust;
+
+public class Background {
+}
---
+++ java/com/akshdeep/joust/MainThread.java
@@ -0,0 +1,13 @@
+package com.akshdeep.joust;
+
+public class MainThread extends Thread {
+    public MainThread() {
+        super();
+
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
