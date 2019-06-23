be2f19f3a2c53d3b4d1db452faad07656cacb576
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-06-22 23:31:15 -0400

    initial commit

---
+++ src/main/java/com/akshdeep/joust/MainActivity.java
@@ -0,0 +1,13 @@
+package com.akshdeep.joust;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
