be7f7e1a286e1993d79d6fbe0b9ade3c8c012fa9
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-06-23 22:52:25 -0400

    too border added

--- java/com/akshdeep/joust/Animation.java
+++ java/com/akshdeep/joust/Animation.java
@@ -2,6 +2,7 @@ package com.akshdeep.joust;
 
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 public class Animation {
     private Bitmap[] frames;
@@ -40,6 +41,32 @@ public class Animation {
 
     public boolean isPlayedOnce() {
         return playedOnce;
+    }
+
+    public static class TopBorder extends  GameObject{
+        private Bitmap resource;
+
+        public TopBorder(Bitmap res, int x, int y, int h) {
+            super.height = h;
+            super.width = 20;
+            super.x = x;
+            super.y = y;
+            super.dx = GamePanel.MOVESPEED;
+            this.resource = Bitmap.createBitmap(res, 0, 0, width, height);
+        }
+
+        public void update() {
+            x += dx;
+        }
+
+        public void draw(Canvas canvas) {
+            try {
+                canvas.drawBitmap(resource, x, y, null);
+
+            } catch (Exception e) {
+            }
+        }
+
     }
 }
 
